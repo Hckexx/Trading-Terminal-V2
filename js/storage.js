344246ec3bd87e9adc1d5de963d767f715a06c3c
Usmaan <168190632+Hckexx@users.noreply.github.com>
2026-06-29 16:35:22 +0530

    Delete js directory

--- js/storage.js
+++
@@ -1,8 +0,0 @@
-export function saveState(data) {
-    localStorage.setItem('tradeTerminal_config', JSON.stringify(data));
-}
-
-export function loadState() {
-    const data = localStorage.getItem('tradeTerminal_config');
-    return data ? JSON.parse(data) : {};
-}
